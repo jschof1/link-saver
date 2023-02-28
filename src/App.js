ad1fe83b7803eaac3db6c9c21200532d1dcd30ce
jschof1 <59843981+jschof1@users.noreply.github.com>
2023-02-28 19:49:25 +0000

    2 files modified - added styles and create new pinboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
+import './style.css'
 import styled from 'styled-components';
 
 const StyledContainer = styled.div`
   display: flex;
   flex-direction: column;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  padding: 10px;
+  margin: 20px;
   width: 400px;
   font-size: 20px;
   font-family: Helvetica Neue;
@@ -16,13 +20,23 @@ const StyledNote = styled.div`
   boxShadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
 `;
 
+
+
 const StyledLink = styled.a`
 text-decoration: none;
 color: inherit;
 outline: 0;
 padding: 10px;
+
 `;
 
+const StyledInput = styled.input`
+border-top-style: hidden;
+border-right-style: hidden;
+border-left-style: hidden;
+border-bottom-style: hidden;
+
+`; 
 const StyledCross = styled.div`
   font-size: 20px;
   padding: 10px;
@@ -58,18 +72,18 @@ const LinkInput = ({ addLink }) => {
   return (
     <StyledNote>
       <span style={{ display: hideLinkInput == true ? 'none' : 'flex' }}>
-        <input
+        <StyledInput
           value={link}
           onChange={handleLinkChange}
-          placeholder="Enter a link here..."
+          placeholder="Enter your link..."
           onKeyPress={handleLinkEnter}
         />
       </span>
       <span style={{ display: hideLinkInput == false ? 'none' : 'flex' }}>
-        <input
+        <StyledInput
           value={name}
           onChange={handleNameChange}
-          placeholder="Enter a name for the link..."
+          placeholder="Give it a name"
           onKeyDown={handleEnter}
         />
       </span>
@@ -77,7 +91,7 @@ const LinkInput = ({ addLink }) => {
   );
 };
 
-export default function App() {
+export default function Pinboard() {
   const [links, setLinks] = useState([]);
 
   const addLink = (link) => {
@@ -110,4 +124,22 @@ export default function App() {
       </StyledContainer>
     </>
   );
+}
+
+export default function App() {
+  const [numPinboards, setNumPinboards] = useState(1)
+  const addPinboard = () =>{
+   setNumPinboards(numPinboards + 1) 
+  }
+
+const pinboards = [];
+for (let i = 1; i <= numPinboards; i++) {
+  pinboards.push(<Pinboard key={i} />);
+}
+  return (
+    <>
+    {pinboards}
+    <button onClick={addPinboard}>+</button>
+    </>
+  )
 }
